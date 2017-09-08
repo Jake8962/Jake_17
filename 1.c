0d4f10f97a5e990d6eb36f9ed07446dcf35a169b
Jake <Jake@Jakeui-MacBook-Pro.local>
2017-09-08 14:52:45 +0900

    0908-1

--- 1.c
+++ 1.c
@@ -1,12 +1,5 @@
 #include "stdio.h"
 
 int main(){
-    int i;
-    scanf("%d",&i);
-    if(i==1)
-	printf("Jake Kim\n");
-    else if(i==2)
-	printf("I study major in Computer science & Engineering!\n");
-
+    printf("Web Application class!\n");
 }
-
